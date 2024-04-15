4dddb893644281c06680084c91a3573dabc27379
Destroyer <s107296@ltisdschools.com>
2024-04-15 16:17:39 -0500

    background and colors

--- Assets/ChangeLight.cs
+++ Assets/ChangeLight.cs
@@ -7,10 +7,12 @@ public class ChangeLight : MonoBehaviour
     public Light l;
     public float returnspeed;
     public Color defaultcolor;
+    public Color changecolor;
     // Start is called before the first frame update
     public void bang()
     {
         l.intensity = 4;
+        l.color = changecolor;
        
     }
     void Update()
